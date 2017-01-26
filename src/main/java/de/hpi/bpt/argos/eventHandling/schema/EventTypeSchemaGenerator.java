0de0b2ce97792bb596edcb6011426026e3b799c2
Johannes Schneider <johannes.schneider@student.hpi.de>
2017-01-26 18:51:52 +0100

    WIP: Implement way to subscribe to unicorn

---
+++ src/main/java/de/hpi/bpt/argos/eventHandling/schema/EventTypeSchemaGenerator.java
@@ -0,0 +1,7 @@
+package de.hpi.bpt.argos.eventHandling.schema;
+
+/**
+ * This interface represents generators which generate event type schema for the event platform.
+ */
+public interface EventTypeSchemeGenerator {
+}
---
+++ src/main/java/de/hpi/bpt/argos/eventHandling/schema/EventTypeSchemaGeneratorImpl.java
@@ -0,0 +1,7 @@
+package de.hpi.bpt.argos.eventHandling.schema;
+
+/**
+ * Created by Johannes Schneider on 26.01.2017.
+ */
+public class EventTypeSchemaGeneratorImpl {
+}
