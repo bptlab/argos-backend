ca3ef6ead8df795be6a37ffcd82b361c42fabd7c
Tom Martensen <tom.martensen@student.hpi.uni-potsdam.de>
2017-02-23 15:11:14 +0100

    use pull request env var

--- scripts/sonarqube.sh
+++ scripts/sonarqube.sh
@@ -8,6 +8,8 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
 else
     echo "Pull request branch"
+    echo $TRAVIS_REPO_SLUG
+    echo $TRAVIS_PULL_REQUEST
     mvn verify sonar:sonar -DskipTests \
         -Dsonar.host.url=$SONAR_HOST_URL \
         -Dsonar.login=$SONAR_AUTH_TOKEN \
@@ -15,5 +17,6 @@ else
         -Dsonar.analysis.mode=preview \
         -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
         -Dsonar.github.oauth=$GITHUB_ACCESS_TOKEN \
+        -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST
 
 fi
