cd4a3c05589a5e438e4484879c69dd11e9f24106
Tom Martensen <tom.martensen@student.hpi.uni-potsdam.de>
2017-02-23 14:39:37 +0100

    letting sonarqube comment on pull requests

---
+++ scripts/sonarqube.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    mvn verify sonar:sonar -DskipTests \
+        -Dsonar.host.url=$SONAR_HOST_URL \
+        -Dsonar.login=$SONAR_AUTH_TOKEN \
+        -Dsonar.branch=$TRAVIS_BRANCH \
+
+else
+    mvn verify sonar:sonar -DskipTests \
+        -Dsonar.host.url=$SONAR_HOST_URL \
+        -Dsonar.login=$SONAR_AUTH_TOKEN \
+        -Dsonar.branch=$TRAVIS_BRANCH \
+        -Dsonar.analysis.mode=preview \
+        -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+        -Dsonar.github.oauth=$GITHUB_ACCESS_TOKEN \
+
+fi
