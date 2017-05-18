0694a84307f3cf2292381513d3a0411eace40009
Christian Friedow <christian@friedow.com>
2017-05-18 11:02:32 +0200

    Corrected environment variables

--- scripts/docker-start.sh
+++ scripts/docker-start.sh
@@ -4,6 +4,6 @@ mvn clean install -DskipTests
 java -jar target/argos-backend.jar \
     -DargosBackendExternalHost=${HOST} \
     -DeventProcessingPlatformHost=${HOST} \
-    -DdatabaseConnectionHost=${MYSQL_PORT_3306_TCP_ADDR}:${MYSQL_PORT_3306_TCP_PORT} \
-    -DdatabaseConnectionUsername=${MYSQL_ENV_MYSQL_USER} \
-    -DdatabaseConnectionPassword=${MYSQL_ENV_MYSQL_PASSWORD}
+    -DdatabaseConnectionHost=database \
+    -DdatabaseConnectionUsername=${MYSQL_USER} \
+    -DdatabaseConnectionPassword=${MYSQL_ROOT_PASSWORD}
