f5349e4a362412d6f068c2f5b779f4536f1ef5a9
Christian Friedow <christian@friedow.com>
2017-05-18 14:31:17 +0200

    Added port to docker start config

--- scripts/docker-start.sh
+++ scripts/docker-start.sh
@@ -4,6 +4,6 @@ mvn clean install -DskipTests
 java -jar target/argos-backend.jar \
     -DargosBackendExternalHost=${HOST} \
     -DeventProcessingPlatformHost=${HOST} \
-    -DdatabaseConnectionHost=database \
+    -DdatabaseConnectionHost=database:3306 \
     -DdatabaseConnectionUsername=${MYSQL_USER} \
     -DdatabaseConnectionPassword=${MYSQL_ROOT_PASSWORD}
